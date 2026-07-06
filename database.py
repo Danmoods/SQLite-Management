29e3957af8af834cedc33a715c7ea612d7e2ebc7
Danmoods <saitamang1234@gmail.com>
2026-07-06 15:26:39 +0300

    Creating a tasks.db database, Created Table and Inserted user entities

---
+++ database.py
@@ -0,0 +1,20 @@
+import sqlite3
+
+# Connect
+conn = sqlite3.connect("tasks.db")
+
+# Cursor
+cursor = conn.cursor()
+
+# Read all tasks
+cursor.execute("SELECT * FROM tasks")
+
+# Store the results
+rows = cursor.fetchall()
+
+# Display each row
+for row in rows:
+    print(row)
+
+# Close database
+conn.close()
